a3c9cbdbe4bdc17d3ab5031537c75412e458c970
Paulo Rauber <pauloeduardorauber@gmail.com>
2025-12-13 09:21:33 +0000

    Remove lemma

--- LeanBandits/ForMathlib/IndepInfinitePi.lean
+++ LeanBandits/ForMathlib/IndepInfinitePi.lean
@@ -21,21 +21,14 @@ variable {ι κ : Type*} {𝓧 : ι → κ → Type*} [m𝓧 : ∀ i j, Measurab
     {μ : (i : ι) → (j : κ) → Measure (𝓧 i j)} [∀ i j, IsProbabilityMeasure (μ i j)]
 
 -- Mathlib/Probability/Independence/InfinitePi.lean
-lemma indep_iSup_infinitePi_infinitePi {S T : Set (ι × κ)} (hd : Disjoint S T) :
-    Indep (⨆ p ∈ S, MeasurableSpace.comap (fun ω ↦ ω p.1 p.2) (m𝓧 p.1 p.2))
-          (⨆ p ∈ T, MeasurableSpace.comap (fun ω ↦ ω p.1 p.2) (m𝓧 p.1 p.2))
-          (infinitePi (fun i ↦ infinitePi (μ i))) :=
-  indep_iSup_of_disjoint (fun _ ↦ Measurable.comap_le (by fun_prop))
-    (iIndepFun_uncurry_infinitePi' (X := fun _ _ ↦ id) μ (by fun_prop)) hd
-
--- Mathlib/Probability/Independence/InfinitePi.lean
 lemma indepFun_proj_infinitePi_infinitePi {a b : κ} (h : a ≠ b) :
     IndepFun (fun ω i ↦ ω i a) (fun ω i ↦ ω i b)
         (infinitePi (fun i ↦ infinitePi (μ i))) := by
   have hd : Disjoint (Set.range fun i : ι ↦ (i, a)) (Set.range fun i ↦ (i, b)) := by
     simp [Set.disjoint_iff_inter_eq_empty, Set.eq_empty_iff_forall_notMem, h.symm]
   simp_rw [IndepFun_iff_Indep, MeasurableSpace.comap_pi]
-  convert indep_iSup_infinitePi_infinitePi (μ := μ) hd
-  all_goals rw [iSup_range]
+  convert indep_iSup_of_disjoint (fun _ ↦ Measurable.comap_le (by fun_prop))
+    (iIndepFun_uncurry_infinitePi' (X := fun _ _ ↦ id) (μ := μ) (by fun_prop)) hd
+  all_goals simp_rw [iSup_range, id_eq]
 
 end ProbabilityTheory

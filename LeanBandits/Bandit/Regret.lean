5769035d9c91f45e7e4935f5932f0a8fe6738fbd
Paulo Rauber <pauloeduardorauber@gmail.com>
2026-01-06 11:35:39 +0000

    Basic properties relating regret and gaps

--- LeanBandits/Bandit/Regret.lean
+++ LeanBandits/Bandit/Regret.lean
@@ -20,12 +20,7 @@ namespace Bandits
 variable {α : Type*} [DecidableEq α] {mα : MeasurableSpace α} {ν : Kernel α ℝ}
   {h : ℕ → α × ℝ} {m n t : ℕ} {a : α}
 
-/-! ### Definitions of regret, gaps, pull counts -/
-
-/-- Regret of a sequence of pulls `k : ℕ → α` at time `t` for the reward kernel `ν ; Kernel α ℝ`. -/
-noncomputable
-def regret (ν : Kernel α ℝ) (t : ℕ) (h : ℕ → α × ℝ) : ℝ :=
-  t * (⨆ a, (ν a)[id]) - ∑ s ∈ range t, (ν (arm s h))[id]
+/-! ### Definitions of gaps, regret, pull counts -/
 
 /-- Gap of an arm `a`: difference between the highest mean of the arms and the mean of `a`. -/
 noncomputable
@@ -35,6 +30,26 @@ omit [DecidableEq α] in
 lemma gap_nonneg [Fintype α] : 0 ≤ gap ν a := by
   rw [gap, sub_nonneg]
   exact le_ciSup (f := fun i ↦ (ν i)[id]) (by simp) a
+
+/-- Regret of a sequence of pulls `k : ℕ → α` at time `t` for the reward kernel `ν ; Kernel α ℝ`. -/
+noncomputable
+def regret (ν : Kernel α ℝ) (t : ℕ) (h : ℕ → α × ℝ) : ℝ :=
+  t * (⨆ a, (ν a)[id]) - ∑ s ∈ range t, (ν (arm s h))[id]
+
+omit [DecidableEq α] in
+lemma regret_eq_sum_gap : regret ν t h = ∑ s ∈ range t, gap ν (arm s h) := by
+  simp [regret, gap]
+
+omit [DecidableEq α] in
+lemma regret_nonneg [Fintype α] : 0 ≤ regret ν t h := by
+  rw [regret_eq_sum_gap]
+  exact sum_nonneg (fun _ _ ↦ gap_nonneg)
+
+omit [DecidableEq α] in
+lemma gap_eq_zero_of_regret_eq_zero [Fintype α] (hr : regret ν t h = 0) {s : ℕ} (hs : s < t) :
+    gap ν (arm s h) = 0 := by
+  rw [regret_eq_sum_gap] at hr
+  exact (sum_eq_zero_iff_of_nonneg fun _ _ ↦ gap_nonneg).1 hr s (mem_range.2 hs)
 
 lemma arm_stepsUntil (hm : m ≠ 0) (h_exists : ∃ s, pullCount a (s + 1) h = m) :
     arm (stepsUntil a m h).toNat h = a := by
@@ -49,7 +64,7 @@ section RewardByCount
 
 lemma regret_eq_sum_pullCount_mul_gap [Fintype α] :
     regret ν t h = ∑ a, pullCount a t h * gap ν a := by
-  simp [sum_pullCount_mul, regret, gap, sum_sub_distrib, arm, action]
+  simp_rw [regret_eq_sum_gap, arm, sum_pullCount_mul, action]
 
 end RewardByCount
 
@@ -72,6 +87,10 @@ lemma gap_eq_bestArm_sub : gap ν a = (ν (bestArm ν))[id] - (ν a)[id] := by
   congr
   refine le_antisymm ?_ (le_ciSup (f := fun a ↦ (ν a)[id]) (by simp) (bestArm ν))
   exact ciSup_le le_bestArm
+
+omit [DecidableEq α] in
+lemma integral_id_eq_of_gap_eq_zero (h : gap ν a = 0) : (ν a)[id] = (ν (bestArm ν))[id] :=
+  (sub_eq_zero.1 (gap_eq_bestArm_sub (a := a) ▸ h)).symm
 
 omit [DecidableEq α] in
 @[simp]

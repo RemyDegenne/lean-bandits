747c29f59a94244379154336e7bc54a44745dd22
Paulo Rauber <pauloeduardorauber@gmail.com>
2026-01-20 11:16:43 +0000

    Organize

--- LeanBandits/BanditAlgorithms/TS.lean
+++ LeanBandits/BanditAlgorithms/TS.lean
@@ -3,79 +3,80 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Paulo Rauber
 -/
-import Mathlib.Probability.Distributions.Uniform
 import LeanBandits.ForMathlib.MeasurableArgMax
 import LeanBandits.SequentialLearning.Algorithm
 
 open MeasureTheory ProbabilityTheory Finset
 open Learning
 
-section StationaryBayesAlgEnvSeq
+section Algorithm -- SequentialLearning/Algorithm.lean
 
 variable {α R : Type*} [mα : MeasurableSpace α] [mR : MeasurableSpace R]
-variable {ℰ : Type*} [mℰ : MeasurableSpace ℰ]
-variable {Ω : Type*} [mΩ : MeasurableSpace Ω]
 
-structure isStationaryBayesAlgEnvSeq
+namespace Learning
+
+def Algorithm.prod_left (E : Type*) [MeasurableSpace E] (alg : Algorithm α R) :
+    Algorithm α (E × R) where
+  policy n := (alg.policy n).comap (fun h i ↦ ((h i).1, (h i).2.2)) (by fun_prop)
+  p0 := alg.p0
+
+variable {Ω E : Type*} [mΩ : MeasurableSpace Ω] [mE : MeasurableSpace E]
+
+def IsPOAlgEnvSeq
     [StandardBorelSpace α] [Nonempty α] [StandardBorelSpace R] [Nonempty R]
-    (alg : Algorithm α R)
-    (Q : Measure ℰ) [IsProbabilityMeasure Q] (κ : Kernel (ℰ × α) R) [IsMarkovKernel κ]
-    (E : Ω → ℰ) (A : ℕ → Ω → α) (R' : ℕ → Ω → R)
-    (P : Measure Ω) [IsFiniteMeasure P] : Prop where
-  measurable_E : Measurable E := by fun_prop
-  measurable_A n : Measurable (A n) := by fun_prop
-  measurable_R n : Measurable (R' n) := by fun_prop
-  hasLaw_env : HasLaw E Q P
-  hasLaw_action_zero : HasLaw (fun ω ↦ (A 0 ω)) alg.p0 P
-  hasCondDistrib_reward_zero : HasCondDistrib (R' 0) (fun ω ↦ (E ω, A 0 ω)) κ P
-  hasCondDistrib_action n :
-    HasCondDistrib (A (n + 1)) (fun ω ↦ (IsAlgEnvSeq.hist A R' n ω, E ω))
-      ((alg.policy n).prodMkRight _) P
-  hasCondDistrib_reward n :
-    HasCondDistrib (R' (n + 1)) (fun ω ↦ (IsAlgEnvSeq.hist A R' n ω, E ω, A (n + 1) ω))
-      (κ.prodMkLeft _) P
+    [StandardBorelSpace E] [Nonempty E]
+    (A : ℕ → Ω → α) (R' : ℕ → Ω → R) (E' : ℕ → Ω → E)
+    (alg : Algorithm α R) (env : Environment α (E × R)) (P : Measure Ω) [IsFiniteMeasure P]
+    := IsAlgEnvSeq A (fun n ω ↦ (E' n ω, R' n ω)) (alg.prod_left E) env P
 
-end StationaryBayesAlgEnvSeq
+end Learning
 
-section Uniform
+end Algorithm
 
-variable {K : ℕ} (hK : 0 < K)
+section StationaryEnv -- SequentialLearning/StationaryEnv.lean
+
+variable {α R E : Type*} [mα : MeasurableSpace α] [mR : MeasurableSpace R] [mE : MeasurableSpace E]
 
 noncomputable
-def uniformAlgorithm : Algorithm (Fin K) ℝ :=
-  have : Nonempty (Fin K) := Fin.pos_iff_nonempty.mp hK
-  { policy _ := Kernel.const _ (PMF.uniformOfFintype (Fin K)).toMeasure
-    p0 := (PMF.uniformOfFintype (Fin K)).toMeasure }
+def BayesStationaryEnv (Q : Measure E) [IsProbabilityMeasure Q] (κ : Kernel (α × E) R)
+    [IsMarkovKernel κ] : Environment α (E × R) where
+  feedback n :=
+    let g : (Iic n → α × (E × R)) × α → (α × E) := fun (h, a) => (a, (h ⟨0, by simp⟩).2.1)
+    (Kernel.deterministic (Prod.snd ∘ g) (by fun_prop)) ×ₖ (κ.comap g (by fun_prop))
+  h_feedback := inferInstance
+  ν0 := (Kernel.const α Q) ⊗ₖ κ
+  hp0 := Kernel.IsMarkovKernel.compProd _ _
 
-end Uniform
+end StationaryEnv
 
-section ThompsonSampling
+section ThompsonSampling -- BanditAlgorithms/TS.lean
 
-variable {K : ℕ} (hK : 0 < K)
-
-variable {ℰ : Type*} [mℰ : MeasurableSpace ℰ] [StandardBorelSpace ℰ] [Nonempty ℰ]
+variable {K : ℕ}
+variable {E : Type*} [mE : MeasurableSpace E] [StandardBorelSpace E] [Nonempty E]
 variable {Ω : Type*} [mΩ : MeasurableSpace Ω]
 
-variable (Q : Measure ℰ) [IsProbabilityMeasure Q] (κ : Kernel (ℰ × (Fin K)) ℝ) [IsMarkovKernel κ]
-variable (E : Ω → ℰ) (A : ℕ → Ω → (Fin K)) (R' : ℕ → Ω → ℝ)
+variable (hK : 0 < K)
+variable (Q : Measure E) [IsProbabilityMeasure Q]
+variable (κ : Kernel (E × Fin K) ℝ) [IsMarkovKernel κ]
+variable (A : ℕ → Ω → Fin K) (R' : ℕ → Ω → ℝ) (E' : ℕ → Ω → E)
 variable (P : Measure Ω) [IsFiniteMeasure P]
 
 noncomputable
-def tsPosterior (n : ℕ) : Kernel (Iic n → (Fin K) × ℝ) ℰ :=
-  condDistrib E (IsAlgEnvSeq.hist A R' n) P
+def tsPosterior (n : ℕ) : Kernel (Iic n → (Fin K) × ℝ) E :=
+  condDistrib (E' 0) (IsAlgEnvSeq.hist A R' n) P
 
 noncomputable
-def isMarkovKernel_tsPosterior (n : ℕ) : IsMarkovKernel (tsPosterior E A R' P n) := by
+def isMarkovKernel_tsPosterior (n : ℕ) : IsMarkovKernel (tsPosterior A R' E' P n) := by
   unfold tsPosterior
   infer_instance
 
 noncomputable
 def tsPolicy (n : ℕ) : Kernel (Iic n → (Fin K) × ℝ) (Fin K) :=
   have : Nonempty (Fin K) := Fin.pos_iff_nonempty.mp hK
-  (tsPosterior E A R' P n).map (measurableArgmax (fun e k ↦ (κ (e, k))[id]))
+  (tsPosterior A R' E' P n).map (measurableArgmax (fun e k ↦ (κ (e, k))[id]))
 
-def isMarkovKernel_tsPolicy (n : ℕ) : IsMarkovKernel (tsPolicy hK κ E A R' P n) := by
-  have : IsMarkovKernel (tsPosterior E A R' P n) := isMarkovKernel_tsPosterior E A R' P n
+def isMarkovKernel_tsPolicy (n : ℕ) : IsMarkovKernel (tsPolicy hK κ A R' E' P n) := by
+  have : IsMarkovKernel (tsPosterior A R' E' P n) := isMarkovKernel_tsPosterior A R' E' P n
   have : Nonempty (Fin K) := Fin.pos_iff_nonempty.mp hK
   apply Kernel.IsMarkovKernel.map
   exact measurable_measurableArgmax fun k =>
@@ -95,9 +96,21 @@ def isProbabilityMeasure_tsInitPolicy : IsProbabilityMeasure (tsInitPolicy hK Q 
 
 noncomputable
 def tsAlgorithm : Algorithm (Fin K) ℝ where
-  policy := tsPolicy hK κ E A R' P
-  h_policy := isMarkovKernel_tsPolicy hK κ E A R' P
+  policy := tsPolicy hK κ A R' E' P
+  h_policy := isMarkovKernel_tsPolicy hK κ A R' E' P
   p0 := tsInitPolicy hK Q κ
   hp0 := isProbabilityMeasure_tsInitPolicy hK Q κ
 
 end ThompsonSampling
+
+-- section Uniform
+
+-- variable {K : ℕ} (hK : 0 < K)
+
+-- noncomputable
+-- def uniformAlgorithm : Algorithm (Fin K) ℝ :=
+--   have : Nonempty (Fin K) := Fin.pos_iff_nonempty.mp hK
+--   { policy _ := Kernel.const _ (PMF.uniformOfFintype (Fin K)).toMeasure
+--     p0 := (PMF.uniformOfFintype (Fin K)).toMeasure }
+
+-- end Uniform

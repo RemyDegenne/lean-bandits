2f4c1831cb98a13c45f165449e337dfc34b5afe3
Remy Degenne <remydegenne@gmail.com>
2026-03-22 20:08:47 +0100

    change to lml

---
+++ verso/Manual/Front.lean
@@ -0,0 +1,22 @@
+import Manual.Pages.DefiningAlgorithm
+import VersoManual
+
+open Verso.Genre Manual Verso.Genre.Manual.InlineLean Verso.Code.External
+
+set_option pp.rawOnError true
+
+set_option verso.exampleProject "../"
+
+set_option verso.exampleModule "LeanMachineLearning"
+
+#doc (Manual) "Lean Machine Learning" =>
+%%%
+authors := ["Rémy Degenne, Paulo Rauber"]
+shortTitle := "Lean Machine Learning"
+%%%
+
+*The Lean Machine Learning library*
+
+TODO
+
+{include 0 Manual.Pages.DefiningAlgorithm}

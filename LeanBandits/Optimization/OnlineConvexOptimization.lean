568ecdc46f66010bd1022355313af67e42edc33f
Remy Degenne <remydegenne@gmail.com>
2026-03-24 09:13:13 +0100

    add todos

--- LeanBandits/Optimization/OnlineConvexOptimization.lean
+++ LeanBandits/Optimization/OnlineConvexOptimization.lean
@@ -20,6 +20,7 @@ variable {E : Type} {mE : MeasurableSpace E}
   [NormedAddCommGroup E] [InnerProductSpace ℝ E] [CompleteSpace E] [BorelSpace E]
   [MeasurableSub₂ E] [SecondCountableTopology E]
   {f : ℕ → E → ℝ} {hf : ∀ n, Measurable (∇ (f n))} {x x₀ : E}
+  {s : Set E} (hs_cvx : Convex ℝ s) (hs_closed : IsClosed s) (hs_nonempty : s.Nonempty)
 
 noncomputable
 def ocoEnv (hf : ∀ n, Measurable (∇ (f n))) : Environment E E where
@@ -27,14 +28,25 @@ def ocoEnv (hf : ∀ n, Measurable (∇ (f n))) : Environment E E where
   ν0 := Kernel.deterministic (fun x ↦ ∇ (f 0) x) (hf 0)
 
 variable (E) in
+/-- Online gradient descent, without projection. -/
 noncomputable
-def sgd (γ : ℕ → ℝ) (x₀ : E) : Algorithm E E :=
+def ogd (γ : ℕ → ℝ) (x₀ : E) : Algorithm E E :=
   detAlgorithm (fun n hist ↦ (hist ⟨n, by grind⟩).1 - γ n • (hist ⟨n, by grind⟩).2) (by fun_prop) x₀
+
+-- TODO: we need the projection on `s`, and we need it to be measurable.
+-- this is a measurable argmin. From measurable selection? Or from a more direct construction,
+-- using the fact that `s` is a countable intersection of half-spaces?
+
+variable (E) in
+/-- Projected online gradient descent. -/
+noncomputable
+def pogd (γ : ℕ → ℝ) (x₀ : E) : Algorithm E E :=
+  detAlgorithm (fun n hist ↦ sorry) sorry x₀
 
 variable {Ω : Type*} {mΩ : MeasurableSpace Ω} {X : ℕ → Ω → E} {G : ℕ → Ω → E}
   {P : Measure Ω} [IsProbabilityMeasure P]
   {γ : ℕ → ℝ}
-  {h_seq : IsAlgEnvSeq X G (sgd E γ x₀) (ocoEnv hf) P}
+  {h_seq : IsAlgEnvSeq X G (ogd E γ x₀) (ocoEnv hf) P}
   {L : ℝ≥0} {hf_smooth : ∀ n, LipschitzWith L (∇ (f n))}
   {hf_convex : ∀ n, ConvexOn ℝ Set.univ (f n)}
   {hγ_pos : ∀ n, 0 < γ n} {hγ_le : ∀ n, γ n ≤ 1 / (4 * L)}

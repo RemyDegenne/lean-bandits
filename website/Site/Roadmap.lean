049dc4d25614463393ce468f054baa4bb405db70
Remy Degenne <remydegenne@gmail.com>
2026-04-11 17:09:17 +0200

    remove copyright headers in website

--- website/Site/Roadmap.lean
+++ website/Site/Roadmap.lean
@@ -1,8 +1,3 @@
-/-
-Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne
--/
 import VersoBlog
 import Site.Meta
 

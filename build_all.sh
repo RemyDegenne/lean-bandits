66fd7f6fac88ea162d0fd00187695ac31789a83b
Remy Degenne <remydegenne@gmail.com>
2026-04-27 13:58:55 +0200

    remove test lines

--- build_all.sh
+++ build_all.sh
@@ -14,8 +14,6 @@ lake build
 lake exe blueprint-gen --output _out/site
 mkdir -p _out/site/html-multi/static
 cp static_files/* _out/site/html-multi/static
-test -f _out/site/html-multi/index.html
-test -f _out/site/html-multi/-verso-data/blueprint-preview-manifest.json
 cd ..
 
 # Copy outputs to home_page

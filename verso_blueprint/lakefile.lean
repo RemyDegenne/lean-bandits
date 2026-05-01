a8ab45b0e5cbd47cdd6b09d73004c13b226c2163
Remy Degenne <remydegenne@gmail.com>
2026-05-01 09:16:11 +0200

    update git source for verso-blueprint

--- verso_blueprint/lakefile.lean
+++ verso_blueprint/lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 require verso from git "https://github.com/leanprover/verso"@"v4.29.0"
-require VersoBlueprint from git "https://github.com/ejgallego/verso-blueprint"
+require VersoBlueprint from git "https://github.com/leanprover/verso-blueprint"
 require LeanMachineLearning from "../"
 
 package LMLBlueprint where
